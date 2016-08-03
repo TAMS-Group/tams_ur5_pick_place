e409d6d5726d4ce6319dc989aa8b7bae7a9f86de
Yannick Jonetzko <5jonetzk@informatik.uni-hamburg.de>
2016-08-03 14:11:32 +0200

    new pick place loop demo, NOT TESTED!
    package is renamed
    
    The new demo should pick the from the object which is detected from the camera and place it somewhere on the table.
    In the moment just a pick loop is implemented, the object will be pickt, the arm will move to the start grap position and then the object will be released.
    
    This demo is not tested yet!

---
+++ src/pick_place_demo.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <stdio.h>
+
+#include <moveit/move_group_interface/move_group.h>
+
+#include <moveit_msgs/CollisionObject.h>
+#include <moveit_msgs/ApplyPlanningScene.h>
+#include <moveit_msgs/Grasp.h>
+
+#include <manipulation_msgs/GraspPlanning.h>
+
+#include <tf/transform_listener.h>
+
+class PickPlaceDemo{
+
+protected:
+    ros::NodeHandle node_handle;
+    ros::ServiceClient planning_scene_diff_client;
+    ros::ServiceClient grasp_planning_service;
+
+    tf::TransformListener tf_listener;
+    tf::StampedTransform transform;
+
+public:
+    PickPlaceDemo(){
+        planning_scene_diff_client = node_handle.serviceClient<moveit_msgs::ApplyPlanningScene>("apply_planning_scene");
+        planning_scene_diff_client.waitForExistence();
+    }
+
+    ~PickPlaceDemo(){
+    }
+
+    bool executePick(){
+
+        moveit::planning_interface::MoveGroup arm("arm");
+        moveit::planning_interface::MoveGroup gripper("gripper");
+        
+        moveit_msgs::Grasp grasp;
+        grasp.id = "grasp";
+
+        jointValuesToJointTrajectory(gripper.getNamedTargetValues("open"), grasp.pre_grasp_posture);
+        jointValuesToJointTrajectory(gripper.getNamedTargetValues("closed"), grasp.grasp_posture);
+
+        geometry_msgs::PoseStamped pose;
+        pose.header.frame_id = "object";
+        pose.pose.orientation.x = 0.5;
+        pose.pose.orientation.y = 0.5;
+        pose.pose.orientation.z = -0.5;
+        pose.pose.orientation.w = 0.5;
+        grasp.grasp_pose = pose;
+
+        grasp.pre_grasp_approach.min_distance = 0.02;
+        grasp.pre_grasp_approach.desired_distance = 0.1;
+        grasp.pre_grasp_approach.direction.header.frame_id = "tool0";
+        grasp.pre_grasp_approach.direction.vector.x = 1.0;
+
+        grasp.post_grasp_retreat.min_distance = 0.02;
+        grasp.post_grasp_retreat.desired_distance = 0.1;
+        grasp.post_grasp_retreat.direction.header.frame_id = "table_top";
+        grasp.post_grasp_retreat.direction.vector.z = 1.0;
+
+        arm.setSupportSurfaceName("table");
+
+        arm.pick("object", grasp);
+
+        return true;
+    }
+
+    
+    //PLACE DOES NOT WORK YET!
+
+/*    void executePlace(){
+
+        moveit::planning_interface::MoveGroup arm("arm");
+        moveit::planning_interface::MoveGroup gripper("gripper");
+
+        std::vector<moveit_msgs::PlaceLocation> location;
+
+        moveit_msgs::PlaceLocation place_location;
+        place_location.id = "place_location";
+
+        jointValuesToJointTrajectory(gripper.getNamedTargetValues("open"), place_location.post_place_posture);
+
+        geometry_msgs::PoseStamped pose;
+        pose.header.frame_id = "table_top";
+        pose.pose.orientation.w = 1;
+        pose.pose.position.x = -0.03;
+        pose.pose.position.y = 0.2;
+        pose.pose.position.z = 0.1;
+        place_location.place_pose = pose;
+
+        place_location.pre_place_approach.min_distance = 0.02;
+        place_location.pre_place_approach.desired_distance = 0.1;
+        place_location.pre_place_approach.direction.header.frame_id = "tool0";
+        place_location.pre_place_approach.direction.vector.x = 1.0;
+
+        place_location.post_place_retreat.min_distance = 0.02;
+        place_location.post_place_retreat.desired_distance = 0.1;
+        place_location.post_place_retreat.direction.header.frame_id = "table_top";
+        place_location.post_place_retreat.direction.vector.z = 1.0;
+
+        location.push_back(place_location);
+
+        arm.setSupportSurfaceName("table");
+        arm.place("object", location);
+    }
+*/
+
+    bool spawnObject(){
+        try{
+            tf_listener.waitForTransform("table_top", "object", ros::Time(0), ros::Duration(10.0));
+            tf_listener.lookupTransform("table_top", "object", ros::Time(0), transform);
+        }catch(tf::TransformException ex){
+            return false;
+        }
+
+        double objectHeight = transform.getOrigin().getZ();
+        moveit_msgs::ApplyPlanningScene srv;
+        moveit_msgs::PlanningScene planning_scene;
+        planning_scene.is_diff = true;
+
+        moveit_msgs::CollisionObject object;
+        object.header.frame_id = "table_top";
+        object.id = "can";
+
+        shape_msgs::SolidPrimitive primitive;
+        primitive.type = primitive.CYLINDER;
+        primitive.dimensions.push_back(objectHeight); //height
+        primitive.dimensions.push_back(0.04); //radius
+        object.primitives.push_back(primitive);
+
+        geometry_msgs::Pose pose;
+        pose.orientation.w = 1.0;
+        pose.position.z = -objectHeight/2;
+        object.primitive_poses.push_back(pose);
+
+        object.operation = object.ADD;
+        planning_scene.world.collision_objects.push_back(object);
+
+        srv.request.scene = planning_scene;
+        planning_scene_diff_client.call(srv);
+
+        return true;
+    }
+
+    bool moveToStart(){
+        moveit::planning_interface::MoveGroup arm("arm");
+
+        arm.setNamedTarget("start_grab_pose");
+        return arm.move();
+    }
+
+    bool releaseObject(){
+        moveit::planning_interface::MoveGroup gripper("gripper");
+
+        gripper.setNamedTarget("closed");
+        return gripper.move();
+    }
+
+    void jointValuesToJointTrajectory(std::map<std::string, double> target_values, trajectory_msgs::JointTrajectory &grasp_pose){
+       grasp_pose.joint_names.reserve(target_values.size());
+       grasp_pose.points.resize(1);
+       grasp_pose.points[0].positions.reserve(target_values.size());
+
+       for(std::map<std::string, double>::iterator it = target_values.begin(); it != target_values.end(); ++it){
+           grasp_pose.joint_names.push_back(it->first);
+           grasp_pose.points[0].positions.push_back(it->second);
+       }
+    }
+
+};
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "PaPTest");
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    PickPlaceDemo demo;
+
+    int count = 0;
+    while(true){
+        count++;
+        ROS_INFO_STREAM("Pick number " << count);
+        if(!demo.spawnObject()){
+            ROS_INFO("No object detected");
+            break;
+        }
+
+        if(!demo.executePick()){
+            ROS_INFO("Pick failed");
+            break;
+        }
+
+        if(!demo.moveToStart()){
+            ROS_ERROR("Move to start pose failed");
+            break;
+        }
+        ros::Duration(5.0).sleep();
+
+        if(!demo.releaseObject()){
+            ROS_ERROR("Release object failed");
+            break;
+        }
+        ros::Duration(10.0).sleep();
+    }
+    return 0;
+}
+
